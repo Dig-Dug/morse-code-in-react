b27a53aaef836a9dc7c5108ec4b41fbf1584750f
unknown <dig-dug>
2021-09-15 00:21:58 +0200

    redo

--- app/src/ks/Abecedary.js
+++ app/src/ks/Abecedary.js
@@ -1,19 +1,27 @@
 import React from 'react';
+import * as Tone from 'tone'
 
 //import SoundHandler from './SoundHandler';
+//create a synth and connect it to the main output (your speakers)
+const synth = new Tone.Synth().toDestination();
+
+//play a middle 'C' for the duration of an 8th note
+//synth.triggerAttackRelease("C4", "8n");
 
 const playAudio = () => {
+    synth.triggerAttackRelease("C4", "8n")
     console.log("ioioioioioio")
   }
 class Abecedary extends React.Component{
    
+
     render(){
         return(
                 
             <div> 
       <button onClick={playAudio}>A</button> 
      
-      <button>E</button> 
+      <button >E</button> 
       <button>I</button> 
       <button>O</button> 
       <button>U</button> 
