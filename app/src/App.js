af1e806eb33660561c34a19bdb493dd573f8ae8e
unknown <dig-dug>
2021-09-14 22:45:06 +0200

    redo

--- app/src/App.js
+++ app/src/App.js
@@ -12,16 +12,14 @@ function App() {
       <header className="App-header">
         <h1>MORSE TRANSLATOR</h1>
         <h2>*under construction * *</h2>
-      
-   
        
-         <img src={logo} className="App-logo" alt="logo" /> 
-        <p>
-          check each character 
-        </p>
         <Container/>
+        <p>check each character</p>
+        <img src={logo} className="App-logo" alt="logo" /> 
       </header>
+     
     </div>
+    
   );
 
 }
--- app/src/ks/Abecedary.js
+++ app/src/ks/Abecedary.js
@@ -1,18 +1,33 @@
 import React from 'react';
 
+//import SoundHandler from './SoundHandler';
 
-
+const playAudio = () => {
+    console.log("ioioioioioio")
+  }
 class Abecedary extends React.Component{
+   
     render(){
         return(
-           
+                
             <div> 
-      <button>A</button> 
+      <button onClick={playAudio}>A</button> 
+     
       <button>E</button> 
       <button>I</button> 
       <button>O</button> 
       <button>U</button> 
-       
+      <button>0</button> 
+      <br/>
+      <button>1</button> 
+      <button>2</button> 
+      <button>3</button>  
+      <button>4</button> 
+      <button>5</button> 
+      <button>6</button> 
+      <button>7</button> 
+      <button>8</button> 
+      <button>9</button> 
          </div>
            
         )
